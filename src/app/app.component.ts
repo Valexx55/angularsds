73352e1a8719bdd6a2b6bfb3c7885845af3e1c7c
val <val@gmail.com>
2024-06-20 13:44:35 +0200

    jueves 20 2 dnicomponent interpolacion ciclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 
@@ -10,6 +10,28 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 //"la parte diánimca o funcional de este componente - JS"
-export class AppComponent {
-  title = 'sdsangular';
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+ 
+  title:string;//non null operator relajamos la restricción sobre null
+  //title!:string;//non null operator relajamos la restricción sobre null
+
+  constructor()
+  {
+    this.title="el curso";
+    console.log("En el constructor de AppComponent");//1
+  }
+  ngOnDestroy(): void {
+    console.log("En el ngOnDestroy de ngOnDestroy"); //el html se ha cargado
+  }
+  ngAfterViewInit(): void {
+    console.log("En el ngAfterViewInit de ngAfterViewInit"); //el html se ha cargado
+  }
+
+  ngOnInit():void
+  {
+    console.log("En el ngOnInit de AppComponent");//2
+  }
+
+
+
 }
--- src/app/components/dni/dni.component.ts
+++ src/app/components/dni/dni.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dni',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './dni.component.html',
   styleUrl: './dni.component.css'
 })
-export class DniComponent {
+export class DniComponent implements OnInit, OnDestroy {
+ 
+  numero!:number;
+  letra!:string;
+  prefijo!:string;
+  /**
+   * TODO: vamos a hacer un componente para el cáculo
+   * de la letra del DNI o 
+   * 
+   * https://www.interior.gob.es/opencms/es/servicios-al-ciudadano/tramites-y-gestiones/dni/calculo-del-digito-de-control-del-nif-nie/
+   */
+ 
+  constructor()
+  {
+    console.log("constructor dni");
+  }
+
+  ngOnInit(): void {
+   console.log("En ngInit Dni");
+  }
+  ngOnDestroy(): void {
+    console.log("En ngOnDestroy Dni");
+  }
 
 }
