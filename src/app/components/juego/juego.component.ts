9a44ad5767156d83e77d5f0f19af909ac77e6702
val <val@gmail.com>
2024-06-25 10:17:20 +0200

    martes 25 viewchildren

--- src/app/components/juego/juego.component.ts
+++ src/app/components/juego/juego.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './juego.component.html',
   styleUrl: './juego.component.css'
 })
-export class JuegoComponent {
+export class JuegoComponent implements AfterViewInit {
 
   nombreJugador:string;
   readonly tituloJuego:string="Juega al Piedra Papel Tijera"
@@ -17,26 +17,113 @@ export class JuegoComponent {
   //defino propiedades para acceder a los hijos del Componente
   //previamente, esos hijos, han sido marcados con la almohadilla #
   //angular, ligará la etiqueta #imgpc a esta propiedad
+  //ElementRef es el tipo de Angular para envolver o referenciar a un elemento "nativo"
   @ViewChild('imgpc') imagenPc!: ElementRef<HTMLImageElement>;
+  @ViewChildren('piedra, papel, tijera') botones!:QueryList<ElementRef<HTMLImageElement>>;
 
+  ids_botones: Array<string> = ["piedra", "papel", "tijera"];
+  img_botones: Array<string> = ["rock", "paper", "scissors"];
 
+  //PARA DECICIDR QUIÉN GANA
+  //[OPCION_JUGADOR, OPCIÓN_MÁQUINA] 
+  //0 --> empate
+  //1--> gana jugador
+  // -1--> gana la máquina
+  tabla_decision: Array<Array<number>> = [ //estructura de datos que permite inferir el resultado
+    [0, -1, 1],
+    [1, 0, -1],
+    [-1, 1, 0]
+  ];
+
+  jugadaUsuario:number;//para saber la jugada 0 1 o 2 -- piedra papel o tijera
+  jugadaPc:number;
 
   constructor()
   {
+    this.jugadaPc = -1;
+    this.jugadaUsuario = -1;
     this.nombreJugador = '';
     this.jugadoPc = false;
     this.jugadoUsuario = false;
+    if (this.imagenPc)
+      {
+        console.log("La imagen existe constructor");
+      } else {
+        console.log("La imagen NO existe constructor");
+      }
+  }
+
+  ngOnInit(): void {
+    
+    console.log("En el ngOnInit de JuegoComponent");
+  }
+
+  //"window.onload () - cuando se carga el HTML"
+  ngAfterViewInit(): void {
+    if (this.imagenPc)
+      {
+        console.log("La imagen existe ngAfterViewInit");
+      } else {
+        console.log("La imagen NO existe ngAfterViewInit");
+      }
+  }
+
+  //generamos un numero aleatorio 0 1 o 2 (piedra papel o tijera)
+  obtenerJugadaAleatoriaPc ():number
+  {
+    return Math.floor(Math.random()*3);
   }
 
   aJugar()
   {
+    //el usuario ya ha hecho su elección, 
+    //determinar la jugada de la máquina
+    this.jugadaPc = this.obtenerJugadaAleatoriaPc();
+    //pintar la imagen de la jugada del pc
+    let rutaDibujo = this.img_botones[this.jugadaPc];
+    this.imagenPc.nativeElement.setAttribute('src', `assets/img/${rutaDibujo}.png`);
+    this.imagenPc.nativeElement.setAttribute('alt', `${rutaDibujo}`);
+
+    this.jugadoPc = true;
+
+    let resultado = this.tabla_decision[this.jugadaUsuario][this.jugadaPc]
+
+    let mensaje:string = '';
+
+    switch (resultado)
+    {
+      case -1: 
+        mensaje = "Ha ganado la máquina";
+      break;
+      case 0: 
+        mensaje = "EMAPTE";
+      break;
+      case 1: 
+        mensaje = "Ha ganado el jugador";
+      break;
+
+    }
+    alert(mensaje);
 
   }
 
 
   seleccionUsuario(numero:number, evento:Event)
   {
+    this.jugadoUsuario = true;
+    this.jugadaUsuario = numero;
+
+    //desmacar / quitar el estilo a las imagenes seleccioanda previamente
+    this.botones.forEach(imgElement => {
+      imgElement.nativeElement.classList.remove('marcada');
+    })
+
+    //vamos a decorar la imagen seleccionada
+    let imagenTocada = <HTMLImageElement>evento.target;// as HTMLImageElement
+    imagenTocada.classList.add('marcada');
 
   }
+
+
 
 }
