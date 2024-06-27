5952589a441821f821209872c424f9472fe0da6e
val <val@gmail.com>
2024-06-27 10:46:05 +0200

    jueves 27 2 todo recuperar lista alumnos servidor y mostrarla en alumnosComponent

--- src/app/components/alumnos/alumnos.component.ts
+++ src/app/components/alumnos/alumnos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Alumno } from '../../models/alumno';
+import { AlumnoService } from '../../services/alumno.service';
 
 @Component({
   selector: 'app-alumnos',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './alumnos.component.html',
   styleUrl: './alumnos.component.css'
 })
-export class AlumnosComponent {
+export class AlumnosComponent implements OnInit {
+
+  listaAlumnos!:Array<Alumno>;
+
+  //TODO: QUIERO CARGAR LA LISTA DE ALUMNOS
+  //y que se muestre en el componente
+
+  constructor (private alumnoService:AlumnoService)
+  {
+    this.listaAlumnos = [];
+    
+  }
+  
+  ngOnInit(): void {
+    //HTTP
+    this.alumnoService.leerTodosLosAlumnosDelServidor()
+  }
+
+
+
+
+
+
 
 }
