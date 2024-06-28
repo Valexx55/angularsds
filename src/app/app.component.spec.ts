347aa1827c4eb8718a27c1adb59ea9d25918ed68
val <val@gmail.com>
2024-06-28 12:50:49 +0200

    viernes 28 3 test app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers:  [provideRouter(routes, withComponentInputBinding())]
     }).compileComponents();
   });
+
+
+   /**
+   * 
+   * abstract class ComponentFixture {
+  debugElement;       // test helper 
+  componentInstance;  // access properties and methods
+  nativeElement;      // access DOM
+  detectChanges();    // trigger component change detection
+}
+
+   */
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +29,31 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sdsangular' title`, () => {
+  it(`should have the SDS Angular title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sdsangular');
+    expect(app.title).toEqual('SDS Angular');
   });
 
-  it('should render title', () => {
+  it('el estilo del logo debe ser angular-logo ', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sdsangular');
+    let appc =  fixture.componentInstance;
+    let domHtmlComponent = fixture.nativeElement as HTMLElement;
+    expect(domHtmlComponent.querySelector('svg')?.classList).toMatch('angular-logo');
+    //fixture.detectChanges();
+    //const compiled = fixture.nativeElement as HTMLElement;
+    //expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sdsangular');
   });
+
+  it('actualización del titulo del componente' , () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    let appc =  fixture.componentInstance;
+    appc.title = "SANTANDER";
+    fixture.detectChanges();
+    let domHtmlComponent = fixture.nativeElement as HTMLElement;
+    expect(domHtmlComponent.querySelector('.navbar-brand')?.innerHTML).toMatch('SANTANDER');
+
+  }
+
+  );
 });
